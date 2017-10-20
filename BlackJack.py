61a6fae88efca9f72a1c3b3b4ea3e5a0cee82460
Satyarth Vats <vatssatyarth11@gmail.com>
2017-10-20 21:41:53 +0530

    First Release

--- BlackJack.py
+++ BlackJack.py
@@ -1,4 +1,8 @@
+# To hit type "Hit"
+# To stay type anything
+
 from random import randint
+import time
 
 class deck(object):
 	deck={"A":4, 2:4, 3:4, 4:4, 5:4, 6:4, 7:4, 8:4, 9:4, 10:4, "J":4, "Q":4, "K":4}
@@ -18,27 +22,99 @@ class deck(object):
 			return self.getCard()
 
 	
-
-
-newDeck=deck()
-print("Player gets following cards")
-playerTotal=0
-# card=newDeck.getCard()
-# print ("First card: "+ str(card))
-# playerTotal=playerTotal+card
-# card=newDeck.getCard()
-# print ("Second Card: "+ str(card))
-for i in range(1, 53):
-	card=newDeck.getCard()
-	print (str(i)+" "+str(card))
-#playerTotal=playerTotal+card
-computerTotal=0
-
 def cardValue(card):
 	global playerTotal
 	cardsDict={"A":1 , 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, "J":10, "Q":10, "K":10 }
 	if card=="A":
 		if playerTotal<=10:
 			return 11
+		else:
+			return 1
 	else:
 		return cardsDict[card]
+
+def action():
+	global playerTotal
+	global newDeck
+	global flag1
+	global flag2
+	time.sleep(2)
+	playerAction=input("Do you hit or stay?")
+	if playerAction=="Hit":
+		card=newDeck.getCard()
+		playerTotal=playerTotal+ cardValue(card)
+		time.sleep(2)
+		print("Your Card: "+str(card))
+		
+		if playerTotal >21:
+			time.sleep(2)
+			print ("You Lose!")
+			flag1=False
+		elif playerTotal==21:
+			#give control to the computer, if he busts you win
+			time.sleep(2)
+			flag2=True
+		else:
+			time.sleep(2)
+			action()
+	
+print("Shuffling Deck")
+time.sleep(2)
+newDeck=deck()
+flag1, flag2= True, False
+playerTotal, computerTotal=0,0
+
+print("Player gets following cards")
+card=newDeck.getCard()
+time.sleep(2)
+
+print ("First card: "+ str(card))
+playerTotal=playerTotal+ cardValue(card)
+card=newDeck.getCard()
+time.sleep(2)
+
+print ("Second Card: "+ str(card))
+playerTotal=playerTotal+ cardValue(card)
+card=newDeck.getCard()
+time.sleep(2)
+
+print ("Dealer's First Card: "+ str(card))
+computerTotal+= cardValue(card)
+card=newDeck.getCard()
+computerTotal+= cardValue(card)
+
+action()
+
+
+if flag1:
+	time.sleep(2)
+	print("Dealer's Second Card: "+str(card))
+	while(computerTotal<21):
+		card=newDeck.getCard()
+		computerTotal+=cardValue(card)
+		time.sleep(2)
+		
+		print("Dealer's Card: "+str(card))
+		
+		if computerTotal>playerTotal and computerTotal<21:
+			time.sleep(2)
+			print ("You lose!")
+			break
+		
+		if computerTotal==21:
+			if flag2:
+				time.sleep(2)
+				print("Its a draw!")
+			else:
+				time.sleep(2)
+				print("You Lose!")
+
+		elif computerTotal> 21:
+			time.sleep(2)
+			print ("You Win!")
+
+	
+
+
+
+
